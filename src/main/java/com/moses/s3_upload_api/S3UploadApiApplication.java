670a2381a3648152bd808f10fdbaa238f247edc9
moses1252 <amviramontes34@yahoo.com>
2025-12-02 22:19:10 -0800

    Initial spring boot project setup

---
+++ src/main/java/com/moses/s3_upload_api/S3UploadApiApplication.java
@@ -0,0 +1,13 @@
+package com.moses.s3_upload_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3UploadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3UploadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moses/s3_upload_api/S3UploadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moses.s3_upload_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3UploadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
