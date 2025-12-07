dc3a13fa01d8a7196f83e2155774d0817a851f1f
moses1252 <amviramontes34@yahoo.com>
2025-12-06 17:21:05 -0800

    Configure database connection and fix authentication isues

--- backend/src/main/java/com/moses/s3_upload_api/S3UploadApiApplication.java
+++ backend/src/main/java/com/moses/s3_upload_api/S3UploadApiApplication.java
@@ -3,11 +3,11 @@ package com.moses.s3_upload_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.amigoscode.customer.Customer;
-import com.amigoscode.customer.CustomerRepository;
-import com.amigoscode.customer.Gender;
-import com.github.javafaker.Faker;
-import com.github.javafaker.Name;
+//import com.amigoscode.customer.Customer;
+//import com.amigoscode.customer.CustomerRepository;
+//import com.amigoscode.customer.Gender;
+//import com.github.javafaker.Faker;
+//import com.github.javafaker.Name;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,27 +24,27 @@ public class S3UploadApiApplication {
 		SpringApplication.run(S3UploadApiApplication.class, args);
 	}
 
-    @Bean
-    CommandLineRunner runner(
-            CustomerRepository customerRepository,
-            PasswordEncoder passwordEncoder) {
-        return args -> {
-            var faker = new Faker();
-            Random random = new Random();
-            Name name = faker.name();
-            String firstName = name.firstName();
-            String lastName = name.lastName();
-            int age = random.nextInt(16, 99);
-            Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
-            String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@amigoscode.com";
-            Customer customer = new Customer(
-                    firstName +  " " + lastName,
-                    email,
-                    passwordEncoder.encode("password"),
-                    age,
-                    gender);
-            customerRepository.save(customer);
-            System.out.println(email);
-        };
-    }
+//    @Bean
+//    CommandLineRunner runner(
+//            CustomerRepository customerRepository,
+//            PasswordEncoder passwordEncoder) {
+//        return args -> {
+//            var faker = new Faker();
+//            Random random = new Random();
+//            Name name = faker.name();
+//            String firstName = name.firstName();
+//            String lastName = name.lastName();
+//            int age = random.nextInt(16, 99);
+//            Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
+//            String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@amigoscode.com";
+//            Customer customer = new Customer(
+//                    firstName +  " " + lastName,
+//                    email,
+//                    passwordEncoder.encode("password"),
+//                    age,
+//                    gender);
+//            customerRepository.save(customer);
+//            System.out.println(email);
+//        };
+//    }
 }
